70689b51b6cd0774ae1b037ad03e1387e7dd3ad9
Ava Pirie <67439021+arpirie@users.noreply.github.com>
2020-07-22 20:43:49 -0400

    Create maze.py

---
+++ maze.py
@@ -0,0 +1,11 @@
+import unittest
+
+class MazeTests(unittest.TestCase):
+    def testItExists(self):
+        m=Maze() # this should fail
+
+class Maze():
+    """ This class creates a random maze """
+
+if __name__=="__main__":
+    unittest.main()
